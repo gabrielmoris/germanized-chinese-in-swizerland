afc59455e334b48437c30a7cb8ab74c19a87202a
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-02-25 15:48:26 +0100

    solve images problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ import Typography from "@mui/material/Typography"
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
-
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
